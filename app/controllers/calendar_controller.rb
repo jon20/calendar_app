54d1ddf8fc633c737e5e559472375961afd451e9
jon20 <kawadu1221@gmail.com>
2017-09-06 14:17:51 +0900

    add create_calender method

--- app/controllers/calendar_controller.rb
+++ app/controllers/calendar_controller.rb
@@ -2,31 +2,12 @@ class CalendarController < ApplicationController
   before_action :sign_in_required, only: [:mainpage]
   def index
       @arr = []
-      ed = 42
       if @date.nil?
         @date = Date.today
       end
-      day = Calendar.where(month: @date.month )
-
-      day.each do |n|
-          @dy = n.date
-          @arr << @dy.day
-      end
-      
-      fg = day.first.date.beginning_of_month
-      fg = fg.wday 
-      fg.times do
-          @arr.unshift(nil)
-      end
-
-      (ed - @arr.length).times do
-        @arr.push(nil)
-      end
+      @arr  =   Calendar.create_calendar(@date)
 
 
-
-      @arr = @arr.each_slice(7).to_a
-      
   end
 
   def show
@@ -34,31 +15,12 @@ class CalendarController < ApplicationController
   end
 
   def next
+      @arr = []
 
       @date = Date.new(params[:year].to_i, params[:month].to_i + 1, 1)
-      ed =42
-      @arr = []
-      if @date.nil?
-        @date = Date.today
-      end
-      day = Calendar.where(month: @date.month )
 
-      day.each do |n|
-          @dy = n.date
-          @arr << @dy.day
-      end
-      
-      fg = day.first.date.beginning_of_month
-      fg = fg.wday 
-      fg.times do
-          @arr.unshift(nil)
-      end
+      @arr = Calendar.create_calendar(@date)
 
-      (ed - @arr.length).times do
-        @arr.push(nil)
-      end
-      
-      @arr = @arr.each_slice(7).to_a
       render template: "/calendar/index"
   end
 
--- app/models/calendar.rb
+++ app/models/calendar.rb
@@ -1,10 +1,29 @@
 class Calendar < ApplicationRecord
-
+  
+  
   def previous
   end
 
-  def next
-    @tmp += 42
-    @ed_end += 42
+  def self.create_calendar(date)
+    arr = []
+    ed =42
+    day = Calendar.where(date: date.beginning_of_month..date.end_of_month)
+    day.each do |n|
+      tmp = n.date
+      arr << tmp.day
+    end
+
+    fg = day.first.date.beginning_of_month
+    fg = fg.wday
+    fg.times do
+      arr.unshift(nil)
+    end
+
+    (ed - arr.length).times do
+      arr.push(nil)
+    end
+    
+    arr = arr.each_slice(7).to_a
+
   end
 end
